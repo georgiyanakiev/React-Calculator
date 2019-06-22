a9a238c5fa4be79c43ff203186507ba6cbc4da55
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2019-06-22 22:27:37 +0100

    Updates

---
+++ precache-manifest.5298fbb3634007546959fc0cd071e8a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b576e9606927774f0688e58571736698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37f1c6bbdf19fe0091cf",
+    "url": "/static/css/main.39738b23.chunk.css"
+  },
+  {
+    "revision": "6ec9ec2c6b694072c227",
+    "url": "/static/js/2.b1cefff5.chunk.js"
+  },
+  {
+    "revision": "37f1c6bbdf19fe0091cf",
+    "url": "/static/js/main.7055f3b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
